d7740a544ca40e5b4023302a16aacfcf551a9149
Aayannoori15 <Aayan.noori15@gmail.com>
2026-06-05 17:21:47 +0530

    clean DSA repo structure

--- CRUD/Stack/stackinheritingsll.py
+++ CRUD/Stack/stackinheritingsll.py
@@ -1,20 +1,20 @@
 from linkedlist.Linkedlist import *
 class stack(SLL):
     def __init__(self):
-        super().__init__(self)
+        super().__init__()
         self.count=0
-    def isempty():
+    def isempty(self):
         return super().is_empty()
     def push(self,data):
         self.insert_at_start(data)
-        count+=1
+        self.count+=1
     def pop(self):
         self.deletefirst()
-        count-=1
+        self.count-=1
     def peek(self):
         if not self.isempty():
             return self.start.item
         else:
             raise IndexError
     def size(self):
-        return self.count
+        return self.count 
