4bba17074b7f4cdc092a3dfa3d90dabe07ef8a47
Aayannoori15 <Aayan.noori15@gmail.com>
2026-06-06 02:40:17 +0530

    priority queue using linked list added

---
+++ CRUD/priority queue/priorityqueueusingll.py
@@ -0,0 +1,38 @@
+class node:
+    def __init__(self,item=None,priority=None,next=None):
+        self.item=item
+        self.priority=priority
+        self.next=next
+class priority_queue:
+    def __init__(self):
+        self.start=None 
+        self.item_count=0
+    def push(self,data,priority):
+        n=node(data,priority)
+        if not self.start or priority<self.start.priority:
+            n.next=self.start 
+            self.start=n
+        else:
+            temp=self.start
+            while temp.next and temp.next.priority <= priority:
+                temp=temp.next 
+            n.next=temp.next 
+            temp.next=n
+        self.item_count+=1
+    def is_empty(self):
+        return self.item_count == 0
+    def size(self):
+        return self.item_count
+    def pop(self):
+        if self.is_empty():
+            raise IndexError
+        elif self.item_count==1:
+            temp=self.start.item
+            self.start=None
+        else :
+            temp=self.start.item 
+            self.start=self.start.next 
+        self.item_count-=1
+        return temp 
+
+        
