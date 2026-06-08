46403d638fd2859234cb9ef3d9da6a9aed55013c
Aayannoori15 <Aayan.noori15@gmail.com>
2026-06-08 06:45:24 +0530

    implemented pre post and in order traversal

--- CRUD/Tree/BSTusingll.py
+++ CRUD/Tree/BSTusingll.py
@@ -16,3 +16,40 @@ class BST:
         elif data>root.item:
             root.right=self.rinsert(root.right,data)
         return root
+    def search(self,data):
+        return self.rsearch(self.root,data)
+    def rsearch(self,root,data):
+        if root == None or root.item == data:
+            return root 
+        if root.item<data:
+            return self.rsearch(root.left,data)
+        elif root.item>data:
+            return self.rsearch(root.right,data)
+    def inorder(self,root):
+        result=[]
+        self.rinorder(self.root,result)
+        return result
+    def rinorder(self,root,result):
+        if root:
+            self.rinorder(root.left,result)
+            result.append(root.item)
+            self.rinorder(root.right,result)
+    def preorder(self,root):
+        result=[]
+        self.rpreorder(self.root,result)
+        return result
+    def rpreorder(self,root,result):
+        if root:
+            result.append(root.item)
+            self.rpreorder(root.left,result)
+            self.rpreorder(root.right,result)
+    def postorder(self,root):
+        result=[]
+        self.rpostorder(self.root,result)
+        return result
+    def rpostorder(self,root,result):
+        if root:
+            result.append(root.item)
+            self.rpostorder(root.left,result)
+            self.rpostorder(root.right,result)
+        
