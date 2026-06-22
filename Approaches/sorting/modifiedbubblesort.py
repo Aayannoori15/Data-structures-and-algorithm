639fd91c6bdb082294478e382fa1d2638cba914d
Aayannoori15 <Aayan.noori15@gmail.com>
2026-06-22 19:45:56 -0400

    modified bubble sort

---
+++ Approaches/sorting/modifiedbubblesort.py
@@ -0,0 +1,9 @@
+def bubble_sort(data_list):
+    for r in range(1,len(data_list)):
+        flag=False
+        for i in range(len(data_list)-r):
+            if data_list[i]>data_list[i+1]:
+                data_list[i],data_list[i+1]=data_list[i+1],data_list[i]
+                flag=True
+            if not flag:
+                break
